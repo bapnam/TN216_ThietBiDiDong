8f323ddd1d8cb45cd88cd2cbb7a76f16032504e6
bapnam <ndnam1411@gmail.com>
2022-02-28 21:55:17 +0700

    create chonmuativi

---
+++ android_X/ChonMuaTiVi/app/src/main/java/thud/chonmuativi/MainActivity.java
@@ -0,0 +1,24 @@
+package thud.chonmuativi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void ChonNgay(View view) {
+    }
+
+    public void DongActivity(View view) {
+    }
+
+    public void ChonMua(View view) {
+    }
+}
