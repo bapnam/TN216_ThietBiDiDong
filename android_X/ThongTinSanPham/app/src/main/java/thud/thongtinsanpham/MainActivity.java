06a491723ee1920af2b9cd04cb17ea851f334bcc
bapnam <ndnam1411@gmail.com>
2022-03-24 16:52:05 +0700

    commit

--- android_X/ThongTinSanPham/app/src/main/java/thud/thongtinsanpham/MainActivity.java
+++ android_X/ThongTinSanPham/app/src/main/java/thud/thongtinsanpham/MainActivity.java
@@ -3,6 +3,7 @@ package thud.thongtinsanpham;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void LuuSanPham(View view) {
+    }
+
+    public void DongActivity(View view) {
+    }
+
+    public void HinhSanPham(View view) {
     }
 }
